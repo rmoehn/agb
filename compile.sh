2fd92866c3de00559818dd882808669b6212b27d
Richard Möhn <richard.moehn@fu-berlin.de>
2014-02-20 20:26:44 +0100

    Kompilierbar gemacht und aktuelles Resultat dazu

--- compile.sh
+++ compile.sh
@@ -3,7 +3,7 @@
 cp glossar.tex glossar.tex.b
 cat glossar.tex.b | perl -pe 's/\\item\[(.*?)]/\\item[$1] \\index{$1}/' > glossar.tex
 latex agb.tex
-bibtex agb
+biber agb
 texindy -L german-duden -M german-sty agb.idx
 #makeindex agb.idx
 latex agb.tex
